1c72d5dbd5ce8ff37a8ef249934f414afa337365
Asma Memon <memonasma73@gmail.com>
2022-12-29 00:03:23 +0530

    add functionality to triangle btn

--- Mathigon.js
+++ Mathigon.js
@@ -1,12 +1,9 @@
-document.addEventListener("DOMContentLoaded",function(){
-    document.querySelector('.Sides').style.display='none';
-    document.querySelector('.Angles').style.display='none';
-    document.querySelector('.Triangle').addEventListener('click', showBtn); 
- 
-    function showBtn(e) {
-        document.querySelector('.Sides').style.display = 'block'; 
-        document.querySelector('.Angles').style.display = 'block';
-        e.preventDefault();  
-    }
+document.querySelector('.Sides').style.display='none';
+document.querySelector('.Angles').style.display='none';
+document.querySelector('.Triangle').addEventListener('click', showBtn); 
 
-});
+function showBtn(e) {
+    document.querySelector('.Sides').style.display = 'block'; 
+    document.querySelector('.Angles').style.display = 'block';
+    e.preventDefault();  
+    }
