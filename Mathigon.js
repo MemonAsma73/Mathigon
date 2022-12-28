0d764ce9f81f2c66eaff6aa35a424ed7f4710350
Asma Memon <memonasma73@gmail.com>
2022-12-28 22:24:26 +0530

    linking js to html

--- Mathigon.js
+++ Mathigon.js
@@ -1,9 +1,3 @@
-document.querySelector('.Sides').style.display='none';
-document.querySelector('.Angles').style.display='none';
-document.querySelector('.Triangle').addEventListener('click', showBtn); 
- 
-function showBtn(e) { 
- document.querySelector('.Sides').style.display = 'block'; 
- document.querySelector('.Angles').style.display = 'block';
- e.preventDefault(); 
-} 
+function log_console() {
+    console.log("Print kr de pls");
+}
